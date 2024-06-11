404aafc7883a85c957b857233c6e0af23e284bf7
kk0055 <rtsw.lgy@gmail.com>
2024-06-11 22:47:06 +0900

    s

--- resources/views/admin/property/components/editModal.blade.php
+++ resources/views/admin/property/components/editModal.blade.php
@@ -1,38 +1,151 @@
-<div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="{{ $property->id }}">
-  <div class="relative w-auto my-6 mx-auto max-w-3xl">
-    <!--content-->
-    <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-      <!--header-->
-      <div class="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-        <h3 class="text-3xl font-semibold">
-          Modal Title
-        </h3>
-        <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onclick="toggleModal({{ $property->id }})">
-          <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-            ×
-          </span>
-        </button>
-      </div>
-      <!--body-->
-      <div class="relative p-6 flex-auto">
-        <p class="my-4 text-blueGray-500 text-lg leading-relaxed">
-          I always felt like I could do anything. That’s the main
-          thing people are controlled by! Thoughts- their perception
-          of themselves! They're slowed down by their perception of
-          themselves. If you're taught you can’t do anything, you
-          won’t do anything. I was taught I could do everything.
-        </p>
-      </div>
-      <!--footer-->
-      <div class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-        <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="toggleModal( {{ $property->id }})">
-          Close
-        </button>
-        <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="toggleModal( {{ $property->id }})">
-          Save Changes
-        </button>
-      </div>
-    </div>
-  </div>
+<div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center"
+    id="{{ $property->id }}">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        action="{{ route('admin.property.update', ['property' => $property->id]) }}" method="POST"
+        enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="relative w-auto my-6 mx-auto max-w-3xl">
+            <!--content-->
+            <div
+                class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <!--header-->
+                <div class="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                    <h3 class="text-3xl font-semibold">
+                        {{ $property->id }}
+                    </h3>
+                    <button
+                        class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                        onclick="toggleModal({{ $property->id }})">
+                        <span
+                            class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                            ×
+                        </span>
+                    </button>
+                </div>
+                <!--body-->
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="relative p-6 flex-auto">
+                    <div class="mb-4">
+                        <div class="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden items-center">
+                            <div class="px-4 py-6">
+                                {{-- Image --}}
+                                <div id="image-preview-{{ $property->id }}"
+                                    class="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer">
+                                    <input id="upload-{{ $property->id }}" type="file" class="hidden" name="image"
+                                        accept="image/*" />
+                                    <label for="upload-{{ $property->id }}" class="cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor"
+                                            class="w-8 h-8 text-gray-700 mx-auto mb-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                                        </svg>
+                                        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload picture
+                                        </h5>
+                                        <span id="filename-{{ $property->id }}"
+                                            class="text-gray-500 bg-gray-200 z-50"></span>
+                                    </label>
+                                    @error('image')
+                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <div class="flex items-center justify-center">
+                                    {{-- <div class="w-full">
+                                        <label
+                                            class="w-full text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center mr-2 mb-2 cursor-pointer">
+                                            <span class="text-center ml-2">Upload</span>
+                                        </label>
+                                    </div> --}}
+                                </div>
+                            </div>
+                        </div>
+                        @error('image')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                        <label class="block text-gray-700 text-sm font-bold mb-4" for="order">
+                            表示順(降順)
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="order" name="order" type="number" value="{{ $property->order }}">
+                    </div>
+                </div>
+                <!--footer-->
+                <div class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                    <button
+                        class="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+                        type="submit">
+                        変更
+                    </button>
+                    <button
+                        class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button" onclick="toggleModal( {{ $property->id }})">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
 <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="{{ $property->id }}-backdrop"></div>
+
+<script>
+    document.querySelectorAll('input[type="file"]').forEach(uploadInput => {
+   
+        const propertyId = uploadInput.id.split('-')[1];
+        const filenameLabel = document.getElementById(`filename-${propertyId}`);
+        const imagePreview = document.getElementById(`image-preview-${propertyId}`);
+       
+       
+        let isEventListenerAdded = false;
+
+        uploadInput.addEventListener('change', (event) => {
+            const file = event.target.files[0];
+            if (file) {
+                filenameLabel.textContent = file.name;
+
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    imagePreview.innerHTML =
+                        `<img src="${e.target.result}" class="max-h-48 rounded-lg mx-auto" alt="Image preview"/>`;
+                    imagePreview.classList.remove('border-dashed', 'border-2', 'border-gray-400');
+
+                    // Add event listener for image preview only once
+                    if (!isEventListenerAdded) {
+                        imagePreview.addEventListener('click', () => {
+                            uploadInput.click();
+                        });
+
+                        isEventListenerAdded = true;
+                    }
+                };
+                reader.readAsDataURL(file);
+            } else {
+                filenameLabel.textContent = '';
+                imagePreview.innerHTML =
+                    `<div class="bg-gray-200 h-48 rounded-lg flex items-center justify-center text-gray-500">No image preview</div>`;
+                imagePreview.classList.add('border-dashed', 'border-2', 'border-gray-400');
+
+                // Remove the event listener when there's no image
+                imagePreview.removeEventListener('click', () => {
+                    uploadInput.click();
+                });
+
+                isEventListenerAdded = false;
+            }
+        });
+
+        uploadInput.addEventListener('click', (event) => {
+            event.stopPropagation();
+        });
+    });
+</script>
