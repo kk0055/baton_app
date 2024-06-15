753719c4b4a424bfc411d8af891df2088c85eb98
kk0055 <rtsw.lgy@gmail.com>
2024-06-15 10:46:36 +0900

    footer

---
+++ resources/views/landing/components/footer.blade.php
@@ -0,0 +1,31 @@
+<footer id="footer">
+    <div id="footer_inner">
+        <p id="footer_logo">
+            <img class="logo_image pc" src="baton_small_logo.jpg" alt="株式会社BATON" title="株式会社BATON" width="196" />
+            <img class="logo_image mobile" src="baton_small_logo.jpg" alt="株式会社BATON" title="株式会社BATON" width="155"
+                height="70" />
+
+        </p>
+        <div id="footer_address" class="post_content clearfix">
+            <p>株式会社BATON<span class="a_break">〒107-0062 東京都港区南青山一丁目1番1号 <span class="a_break">新青山ビル東館
+                        7F</span></p>
+        </div>
+        <div id="footer_address" class="post_content clearfix">
+            <p>物件に関するお問い合わせは <span><a href="https://docs.google.com/forms/d/1OGoRCunJpDpYy6aYS-DGVoTodgmLvII9Qz4A9ipEYyc/viewform?edit_requested=true"
+              target="_blank">こちら</a></span> よりお気軽にご連絡ください。</p>
+        </div>
+        <ul id="footer_sns" class="sns_button_list clearfix color_type1">
+            {{-- <li > <a href="https://www.instagram.com/baton_2024/" rel="nofollow noopener" target="_blank" title="Instagram">
+              <img src="Instagram_icon.png" alt="Instagram Icon" style="width: 40px; height: 40px;">
+          </a></li> --}}
+            <a href="https://www.instagram.com/baton_2024/" target="_blank">
+                <img src="inst_banner.png" alt="" height="50"></a>
+            {{-- <li class="twitter"><a href="https://twitter.com/tcd_jp" rel="nofollow noopener" target="_blank"
+                  title="X"><span>X</span></a></li>
+          <li class="youtube"><a href="https://www.youtube.com/channel/UCF2WVjA5eHLue-3cZ5UBUsw/videos"
+                  rel="nofollow noopener" target="_blank" title="Youtube"><span>Youtube</span></a></li> --}}
+        </ul>
+
+    </div>
+    <p id="copyright"><span>© BATON inc.</span></p>
+</footer>
