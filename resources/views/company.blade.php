4940b25909791933e4e9d6179a671b8b4e7932f7
kk0055 <rtsw.lgy@gmail.com>
2024-11-03 14:32:39 +0900

    meta

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <meta name="description" content="株式会社BATON">
-    <title>株式会社BATON 世田谷のおすすめ部屋探し | 人気物件紹介＆見積もり自信有り</title>
+    <title>株式会社BATON 世田谷のおすすめ部屋探し | 人気物件紹介＆初期費用自信有り</title>
     <meta name='robots' content='max-image-preview:large' />
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://xbatonx.com">
     <meta property="og:title" content="株式会社BATON">
-    <meta name="description" content="世田谷で人気のおすすめ物件を探すなら、私たちにお任せください。見積もりに自信があり、Webに掲載されている物件も紹介可能です。理想の部屋探しをお手伝いします。">
-    <meta name="keywords" content="世田谷, 部屋探し, 賃貸, 物件, 人気, おすすめ, 見積もり, Web掲載, 不動産, 賃貸物件, 引越し, 家探し, 世田谷賃貸">
+    <meta name="description" content="小田急線、田園都市線の沿線など世田谷で人気のおすすめ物件を探すなら、私たちにお任せください。初期費用に自信があり、Webに掲載されている物件はほぼ紹介可能です。理想の部屋探しをお手伝いします。">
+    <meta name="keywords" content="世田谷, 部屋探し, 賃貸, 物件, 人気, おすすめ, 初期費用,仲介手数料無料, Web掲載, 不動産, 賃貸物件, 引越し, 家探し, 世田谷賃貸,小田急線.田園都市線,京王線,世田谷線">
     <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:site_name" content="株式会社BATON">
--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -9,9 +9,9 @@
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://xbatonx.com">
     <meta property="og:title" content="株式会社BATON">
-    <title>株式会社BATON 世田谷のおすすめ部屋探し | 人気物件紹介＆見積もり自信有り</title>
-    <meta name="description" content="世田谷で人気のおすすめ物件を探すなら、私たちにお任せください。見積もりに自信があり、Webに掲載されている物件も紹介可能です。理想の部屋探しをお手伝いします。">
-    <meta name="keywords" content="世田谷, 部屋探し, 賃貸, 物件, 人気, おすすめ, 見積もり, Web掲載, 不動産, 賃貸物件, 引越し, 家探し, 世田谷賃貸">
+    <title>株式会社BATON 世田谷のおすすめ部屋探し | 人気物件紹介＆初期費用自信有り</title>
+    <meta name="description" content="小田急線、田園都市線の沿線など世田谷で人気のおすすめ物件を探すなら、私たちにお任せください。初期費用に自信があり、Webに掲載されている物件はほぼ紹介可能です。理想の部屋探しをお手伝いします。">
+    <meta name="keywords" content="世田谷, 部屋探し, 賃貸, 物件, 人気, おすすめ, 初期費用,仲介手数料無料, Web掲載, 不動産, 賃貸物件, 引越し, 家探し, 世田谷賃貸,小田急線.田園都市線,京王線,世田谷線">
     <meta property="og:site_name" content="株式会社BATON">
     <meta property="og:image:secure_url" content="https://xbatonx.com">
     <meta property="og:image:width" content="1200">
