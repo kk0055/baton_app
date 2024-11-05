a39e4b07f918d74f05b108c7155b41d8f2af787c
kk0055 <rtsw.lgy@gmail.com>
2024-11-05 21:51:26 +0900

    不要なcss削除

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -2,26 +2,9 @@
 <html class="pc" lang="ja">
 
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="description" content="株式会社BATON">
-    <title>株式会社BATON 世田谷のおすすめ部屋探し | 人気物件紹介＆初期費用自信有り</title>
+    
+    @include('partials.meta')
 
-    <meta name='robots' content='max-image-preview:large' />
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://xbatonx.com">
-    <meta property="og:title" content="株式会社BATON">
-    <meta name="description"
-        content="小田急線、田園都市線の沿線など世田谷で人気のおすすめ物件を探すなら、私たちにお任せください。初期費用に自信があり、Webに掲載されている物件はほぼ紹介可能です。理想の部屋探しをお手伝いします。">
-    <meta name="keywords"
-        content="世田谷, 部屋探し, 賃貸, 物件, 人気, おすすめ, 初期費用,仲介手数料無料, Web掲載, 不動産, 賃貸物件, 引越し, 家探し, 世田谷賃貸,小田急線.田園都市線,京王線,世田谷線">
-    <meta name="robots" content="index, follow">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:site_name" content="株式会社BATON">
-    <meta property="og:image:secure_url" content="https://xbatonx.com">
-    <meta property="og:image:width" content="1200">
-    <meta property="og:image:height" content="630">
-    <meta name="google-site-verification" content="Ea3oV6nXXDt2fhyJBsoyrTR1EdnDAt-ZaNG_KY-85Bk" />
     <link rel='dns-prefetch' href='//cdn.jsdelivr.net' />
     <link rel='stylesheet' id='swiper-css'
         href='https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css?ver=10.2.0' type='text/css' media='all' />
