110be9215bac8b7c9593c69e62b90d2ae640880d
kk0055 <rtsw.lgy@gmail.com>
2024-11-13 19:19:30 +0900

    s

--- resources/views/layout/landing/app.blade.php
+++ resources/views/layout/landing/app.blade.php
@@ -8,28 +8,7 @@
     <link rel='dns-prefetch' href='//cdn.jsdelivr.net' />
     <link rel='stylesheet' id='swiper-css' href='https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css?ver=10.2.0'
         type='text/css' media='all' />
-    {{-- <link rel='stylesheet' id='main-style-css'
-        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/style.css?ver=6.5.4' type='text/css'
-        media='all' />
-    <link rel='stylesheet' id='design-plus-css'
-        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/css/design-plus.css?ver=1.0'
-        type='text/css' media='all' />
-    <link rel='stylesheet' id='sns-botton-css'
-        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/css/sns-botton.css?ver=1.0'
-        type='text/css' media='all' />
-    <link rel='stylesheet' id='responsive-css'
-        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/css/responsive.css?ver=1.0'
-        type='text/css' media='screen and (max-width:1391px)' />
-    <link rel='stylesheet' id='wp-block-library-css'
-        href='https://demo.tcd-theme.com/tcd103/wp-includes/css/dist/block-library/style.min.css?ver=6.5.4'
-        type='text/css' media='all' /> --}}
 
-    {{-- <link rel='stylesheet' id='child-style-css'
-        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/GENESIS-child/style.css?ver=6.5.4' type='text/css'
-        media='all' />
-    <link rel='stylesheet' id='hcb-style-css'
-        href='https://demo.tcd-theme.com/tcd103/wp-content/plugins/highlighting-code-block/build/css/hcb--light.css?ver=2.0.1'
-        type='text/css' media='all' /> --}}
     <script type="text/javascript" src="https://demo.tcd-theme.com/tcd103/wp-includes/js/jquery/jquery.min.js?ver=3.7.1"
         id="jquery-core-js"></script>
     <script type="text/javascript"
---
+++ resources/views/layouts/backup.blade.php
@@ -0,0 +1,22 @@
+    {{-- <link rel='stylesheet' id='main-style-css'
+        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/style.css?ver=6.5.4' type='text/css'
+        media='all' />
+    <link rel='stylesheet' id='design-plus-css'
+        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/css/design-plus.css?ver=1.0'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='sns-botton-css'
+        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/css/sns-botton.css?ver=1.0'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='responsive-css'
+        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/genesis_tcd103/css/responsive.css?ver=1.0'
+        type='text/css' media='screen and (max-width:1391px)' />
+    <link rel='stylesheet' id='wp-block-library-css'
+        href='https://demo.tcd-theme.com/tcd103/wp-includes/css/dist/block-library/style.min.css?ver=6.5.4'
+        type='text/css' media='all' /> --}}
+
+    {{-- <link rel='stylesheet' id='child-style-css'
+        href='https://demo.tcd-theme.com/tcd103/wp-content/themes/GENESIS-child/style.css?ver=6.5.4' type='text/css'
+        media='all' />
+    <link rel='stylesheet' id='hcb-style-css'
+        href='https://demo.tcd-theme.com/tcd103/wp-content/plugins/highlighting-code-block/build/css/hcb--light.css?ver=2.0.1'
+        type='text/css' media='all' /> --}}
