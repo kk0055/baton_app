35d13c89e5c70cc5694f5b45db1b3f1582306e67
kk0055 <rtsw.lgy@gmail.com>
2024-11-21 13:52:33 +0900

    仮決定

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use App\Models\Property;
+use App\Models\RailwayLine;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
@@ -11,7 +11,7 @@ class LandingPageController extends Controller
     public function index()
     {
         $rents = Property::rentOnly()->take(10)->get();
-         
+
         return view('index', compact('rents'));
     }
 
@@ -23,44 +23,55 @@ class LandingPageController extends Controller
 
     public function rent(Request $request)
     {
-        $price = $request->input('price');
-        $is_new_building = $request->get('is_new_building');
-        $is_brokerage_free = $request->get('is_brokerage_free');
-        $propertyPrices = Property::propertyPrices();
-        $query = Property::rentOnly();
+        $railwayLines = RailwayLine::get();
+        // チェックボックスで選択された路線IDを取得
+        $selectedRailways = $request->input('railways', []);
 
-        // 価格帯が指定されている場合の処理
-        if ($price && $price != '指定無し') {
-            $query->where('price', '=', $price);
+        // 結果を格納する配列
+        $result = [];
+        $rent = [];
+        if (!empty($selectedRailways)) {
+            // 選択された路線ごとにデータを取得
+            foreach ($selectedRailways as $railwayId) {
+                $railway = RailwayLine::with(['properties' => function ($query) {
+                    $query->orderBy('properties.created_at', 'desc');
+                }])->find($railwayId);
+
+                if ($railway) {
+                    $result[$railway->name] = $railway->properties;
+                }
+            }
+        } else {
+            // 選択されていない場合はすべての物件を新しい順に
+            $rent = Property::orderBy('created_at', 'desc')->get();
         }
 
-        // 新築のチェックがある場合の処理
-        if ($is_new_building == '1') {
-            $query->where('is_new_building', true);
+        if (isset($result['指定無し']) && $result['指定無し']) {
+            $rent = Property::orderBy('created_at', 'desc')->get();
         }
+        $denentoshi = isset($result['田園都市線']) ? $result['田園都市線'] : null;
+        $odakyu = isset($result['小田急線']) ? $result['小田急線'] : null;
+        $setagaya = isset($result['世田谷線']) ? $result['世田谷線'] : null;
+        $toyoko = isset($result['東横線']) ? $result['東横線'] : null;
+        $keio = isset($result['京王線']) ? $result['京王線'] : null;
 
-        // 仲介手数料無料のチェックがある場合の処理
-        if ($is_brokerage_free == '1') {
-            $query->where('is_brokerage_free', true);
-        }
-
-        // フィルタリング結果を取得
-        $rents = $query->get();
-        return view('rent', compact('rents','propertyPrices'));
+    //     return view('rent', compact('rent', 'railwayLines', 'denentoshi', 'odakyu', 'setagaya', 'toyoko', 'keio'));
+    // }
+        return view('rent', compact('rent', 'result','railwayLines', 'denentoshi', 'odakyu', 'setagaya', 'toyoko', 'keio'));
     }
 
     public function isBrokerageFree()
     {
         $rents = Property::rentOnly()->where('is_brokerage_free', true)->get();
         $propertyPrices = Property::propertyPrices();
-        return view('rent', compact('rents','propertyPrices'));
+        return view('rent', compact('rents', 'propertyPrices'));
     }
-    
+
     public function company()
     {
         return view('company');
     }
-    
+
     public function message()
     {
         return view('message');
--- resources/views/rent.blade.php
+++ resources/views/rent.blade.php
@@ -1,8 +1,43 @@
 @extends('layout.landing.app')
 
 @section('title', '新着物件 | 株式会社BATON 世田谷のおすすめ部屋探し | 人気物件紹介＆初期費用自信有り')
-@section('description', '世田谷エリアの最新賃貸物件情報をチェック！小田急線や田園都市線沿線の新着物件をご紹介。初期費用を抑えたお得な物件を掲載中。世田谷区を中心としてはおりますが、都内を中心に首都圏どこでもご紹介可能です。 もっと多くの選択肢から物件をご紹介できますので、気になる条件があればお気軽にお問い合わせください！')
+@section('description',
+    '世田谷エリアの最新賃貸物件情報をチェック！小田急線や田園都市線沿線の新着物件をご紹介。初期費用を抑えたお得な物件を掲載中。世田谷区を中心としてはおりますが、都内を中心に首都圏どこでもご紹介可能です。
+    もっと多くの選択肢から物件をご紹介できますので、気になる条件があればお気軽にお問い合わせください！')
 @section('canonical', 'https://xbatonx.com/rent')
+@section('styles')
+    <style>
+        .railway-options {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;
+            justify-content: flex-start;
+        }
+
+        .railway-option {
+            display: flex;
+            align-items: center;
+            gap: 5px;
+        }
+
+        .search-button {
+            margin-left: 4px
+        }
+        .railway-title {
+            margin-top:20px;
+            color:#507878;
+            font-weight: bold;
+
+
+        }
+        /* スマホ用のスタイル（横幅 768px 以下の端末に適用） */
+        @media screen and (max-width: 768px) {
+            .search-button {
+                margin-left: 40px;
+            }
+        }
+    </style>
+@endsection
 @section('bodyClass', 'blog wp-embed-responsive megamenu_dark_color')
 @section('content')
     <div id="js-body-start"></div>
@@ -11,70 +46,78 @@
 
     <div id="archive_header" class="inview" style="margin-top: 50px;">
         <div class="title_area ">
-            @if (Route::currentRouteName() == 'landing.isBrokerageFree')
-                <h2 class="catch large_headline"><span>仲介手数料無料</span></h2>
-            @else
             <h4 style="font-size: 40px; margin-bottom:20px;" class="colored catch ">新着物件</h4>
-            @endif
             {{-- <p class="sub_title colored"><span></span></p> --}}
         </div>
         <div class=" inview slide_up_animation" style="line-height:2;">
-        <p class="" style="margin-bottom: 5px;">
-            大手不動産会社と同じデータベースを使用しておりますので、他社サイトで見つけた物件もご紹介が可能です。
-        </p>
-        <p class="" style="margin-bottom: 5px;">
-            世田谷区を中心としてはおりますが、都内を中心に首都圏どこでもご紹介可能です。
-        </p>
-        <p class="" style="margin-bottom: 5px;">
-            当サイトに掲載している物件は、取り扱い物件のほんの一部です。
-        </p>
-        <p class="">
-            もっと多くの選択肢から物件をご紹介できますので、気になる条件があればお気軽にお問い合わせください！
-        </p>
-    </div>
+            <p class="" style="margin-bottom: 5px;">
+                大手不動産会社と同じデータベースを使用しておりますので、他社サイトで見つけた物件もご紹介が可能です。
+            </p>
+            <p class="" style="margin-bottom: 5px;">
+                世田谷区を中心としてはおりますが、都内を中心に首都圏どこでもご紹介可能です。
+            </p>
+            <p class="" style="margin-bottom: 5px;">
+                当サイトに掲載している物件は、取り扱い物件のほんの一部です。
+            </p>
+            <p class="">
+                もっと多くの選択肢から物件をご紹介できますので、気になる条件があればお気軽にお問い合わせください！
+            </p>
+        </div>
         {{-- <p class="desc post_content">今週のおススメ物件</p> --}}
     </div>
 
     <section id="archive_blog" class="slide_up_animation">
         {{-- 検索 --}}
-        {{-- @if (Route::currentRouteName() !== 'landing.isBrokerageFree')
-            <div class="sub_title colored property "
-                style="font-size: 95%; color:#000000; padding:10px 0; margin-bottom:30px;">
-                <form action="{{ route('landing.rent') }}" method="GET" style="">
-                    <select name="price" id="price"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        style="font-weight: bold; background-color:#699999; color: white; padding:10px;margin-right:7px;">
-                        @foreach ($propertyPrices as $price)
-                            <option value="{{ $price }}" style="font-weight: bold;">
-                                {{ $price }}
-                            </option>
+        <div class="sub_title colored property " style="font-size: 95%; color:#000000; padding:10px 0; margin-bottom:30px;">
+            <form action="{{ route('landing.rent') }}" method="GET" style="">
+                {{-- <select name="price" id="price"
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    style="font-weight: bold; background-color:#699999; color: white; padding:10px;margin-right:7px;">
+                    @foreach ($propertyPrices as $price)
+                        <option value="{{ $price }}" style="font-weight: bold;">
+                            {{ $price }}
+                        </option>
+                    @endforeach
+                </select> --}}
+                <div style="margin-top: 10px; display: flex; align-items: center; margin-right: 8px; font-size: 1.1em;">
+                    <div class="railway-options">
+                        @foreach ($railwayLines as $railway)
+                            <div class="railway-option">
+                                <input id="railway-{{ $railway->id }}" type="checkbox" name="railways[]"
+                                    style="margin-right:5px; width: 20px; height: 20px" value="{{ $railway->id }}">
+                                <label for="railway-{{ $railway->id }}"
+                                    class="block text-gray-700 text-sm font-bold mb-2 mt-4"
+                                    style="cursor: pointer; ">{{ $railway->name }}</label>
+                            </div>
                         @endforeach
-                    </select>
-                    <div style="margin-top: 10px; display: flex; align-items: center;">
-                        <div style="display:flex; align-items: center; margin-right:4px;">
-                            <input type="checkbox" name="is_new_building" id="is_new_building" value="1"
-                                style="margin-right:5px;" {{ request()->get('is_new_building') == '1' ? 'checked' : '' }}>
-                            <label for="is_new_building" class="block text-gray-700 text-sm font-bold mb-2 mt-4">新築</label>
-                        </div>
-
-                        <div style="display:flex; align-items: center; margin-right:4px;">
-                            <input type="checkbox" name="is_brokerage_free" id="is_brokerage_free" value="1"
-                                style="margin-right:5px;" {{ request()->get('is_brokerage_free') == '1' ? 'checked' : '' }}>
-                            <label for="is_brokerage_free"
-                                class="block text-gray-700 text-sm font-bold mb-2 mt-4">仲介手数料無料</label>
-                        </div>
-
-                        <button type="submit" class="catch property_search" style="padding:3px; margin-left:4px; ">
+                        <button type="submit" class="search-button catch property_search" style="padding: 6px;">
                             <span class="label" style="font-size: 16px;"><i class="fa fa-search" style="margin-right:4px;"
                                     aria-hidden="true"></i>検索</span>
                         </button>
                     </div>
-                </form>
-            </div>
-        @endif --}}
+                    {{-- <div style="display:flex; align-items: center; margin-right:4px;">
+                        <input type="checkbox" name="is_new_building" id="is_new_building" value="1"
+                            style="margin-right:5px;" {{ request()->get('is_new_building') == '1' ? 'checked' : '' }}>
+                        <label for="is_new_building" class="block text-gray-700 text-sm font-bold mb-2 mt-4">新築</label>
+                    </div> --}}
+
+                    {{-- <div style="display:flex; align-items: center; margin-right:4px;">
+                        <input type="checkbox" name="is_brokerage_free" id="is_brokerage_free" value="1"
+                            style="margin-right:5px;" {{ request()->get('is_brokerage_free') == '1' ? 'checked' : '' }}>
+                        <label for="is_brokerage_free"
+                            class="block text-gray-700 text-sm font-bold mb-2 mt-4">仲介手数料無料</label>
+                    </div> --}}
+
+                    {{-- <button type="submit" class="catch property_search" style="padding:3px; margin-left:4px; ">
+                        <span class="label" style="font-size: 16px;"><i class="fa fa-search" style="margin-right:4px;"
+                                aria-hidden="true"></i>検索</span>
+                    </button> --}}
+                </div>
+            </form>
+        </div>
         {{-- 検索 --}}
         <div class="blog_list">
-            @foreach ($rents as $property)
+            @foreach ($rent as $property)
                 <div class="item">
                     <a class="image_link image-link animate_background"
                         href="{{ asset('storage/' . $property->image_path) }}">
@@ -85,14 +128,55 @@
                         </div>
                     </a>
                     {{-- <p style="margin-bottom:10px">価格帯: {{ $property->price }}</p>
-                    <p style="margin-bottom:10px">新築: {{ $property->is_new_building ? '✅' : '❌' }} </p>
-                    <p style="margin-bottom:10px">仲介手数料無料: {{ $property->is_brokerage_free ? '✅' : '❌' }} </p> --}}
+            <p style="margin-bottom:10px">新築: {{ $property->is_new_building ? '✅' : '❌' }} </p>
+            <p style="margin-bottom:10px">仲介手数料無料: {{ $property->is_brokerage_free ? '✅' : '❌' }} </p> --}}
                     {{-- <div class="content">
-                    <time class="date entry-date published" datetime="2023-09-28T23:21:04+09:00">2023.09.28</time>
-                </div> --}}
+            <time class="date entry-date published" datetime="2023-09-28T23:21:04+09:00">2023.09.28</time>
+            </div> --}}
                 </div>
             @endforeach
         </div>
+        {{-- 田園都市線 --}}
+        @if ($denentoshi)
+            <h1 class="railway-title">田園都市線</h1>
+            <div class="blog_list">
+                @foreach ($denentoshi as $property)
+                    <div class="item">
+                        <a class="image_link image-link animate_background"
+                            href="{{ asset('storage/' . $property->image_path) }}">
+                            <div class="image_wrap">
+                                <img loading="lazy" class="image" src="{{ asset('storage/' . $property->image_path) }}"
+                                    alt="Property Image" width="770" height="520"
+                                    style="width: 100%; height: 100%; object-fit: contain;" />
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+
+
+        {{-- 小田急線 --}}
+        @if ($odakyu)
+            <h1 class="railway-title">小田急線</h1>
+            <div class="blog_list">
+                @foreach ($odakyu as $property)
+                    <div class="item">
+                        <a class="image_link image-link animate_background"
+                            href="{{ asset('storage/' . $property->image_path) }}">
+                            <div class="image_wrap">
+                                <img loading="lazy" class="image" src="{{ asset('storage/' . $property->image_path) }}"
+                                    alt="Property Image" width="770" height="520"
+                                    style="width: 100%; height: 100%; object-fit: contain;" />
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+
+
+        {{-- </div> --}}
 
     </section>
     <!-- 画像をクリックしたときに表示されるモーダル -->
